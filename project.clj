1f304383cadf01065f3305922cdab56f5849754b
Dan Pomohaci <dan.pomohaci@gmail.com>
2018-03-01 07:09:44 +0200

    add hive jdbc

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ejc-sql "0.0.1-SNAPSHOT"
+(defproject ejc-sql "0.1-dev01"
   :description "Interact with database via java/clojure libs to run SQL/JPQL scripts"
   :url "https://github.com/kostafey/ejc-sql"
   :license {:name "GPL 2.0+"
@@ -11,6 +11,10 @@
                  [com.cemerick/pomegranate "0.3.0" :exclusions [org.codehaus.plexus/plexus-utils]]
                  [org.apache.commons/commons-lang3 "3.2.1"]
                  [mysql/mysql-connector-java "8.0.9-rc"]
+                 ;; https://mvnrepository.com/artifact/org.apache.hive/hive-jdbc
+                 [org.apache.hive/hive-jdbc "2.3.2" :exclusions [org.apache.hadoop/hadoop-yarn-common
+                                                                 jdk.tools
+                                                                 commons-collections]]
                  [org.postgresql/postgresql "42.2.1.jre7"]]
   :pedantic? :warning
   :plugins [[lein-ancient "0.6.15" :exclusions [org.clojure/clojure]]]
